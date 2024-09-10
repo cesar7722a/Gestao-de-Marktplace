65eef4c83ab2ec99f170b50a9d144cdcfc322c77
César Filipe António <cesarfilipeantonio2404@gmail.com>
2024-09-10 20:44:24 +0100

    feat:adicionando page Login

---
+++ src/pages/Login.tsx
@@ -0,0 +1,15 @@
+export function Login() {
+  return (
+    <div className="bg-[#FBF4F4] h-screen">
+      <div className="w-[1366px]">
+        <div className="pt-8 pl-8">
+          <img
+            src="../public/Title.png"
+            alt="logo gestão marketplace"
+            className=""
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
