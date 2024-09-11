ba605c58b42e1ccfabd11ab1c256d06e0cf4f084
César Filipe António <cesarfilipeantonio2404@gmail.com>
2024-09-11 21:41:45 +0100

    chose:estilizando llabel emaol

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,7 +2,7 @@ export function Login() {
   return (
     <div className="h-screen">
       <div className="">
-        <div className="flex">
+        <div className="flex items-center">
           <div>
             <figure className="pt-8 pl-8">
               <img src="../public/Title.png" alt="logo gestão marketplace" />
@@ -14,9 +14,9 @@ export function Login() {
               />
             </figure>
           </div>
-          <div className="p-6 max-w-2xl bg-white">
+          <div className="p-6 max-w-2xl bg-white rounded-[32px]">
             <div className="px-20 py-[72px]">
-              <div>
+              <div className="space-y-8">
                 <div className="space-y-2">
                   <h1 className="text-2xl text-[#1D1D1D] font-semibold">
                     Acesse sua conta
@@ -25,6 +25,16 @@ export function Login() {
                     Informe seu e-mail e senha para entrar
                   </h2>
                 </div>
+                <form action="">
+                  <span className="flex flex-col">
+                    <label htmlFor="" className="text-[#666666] text-xs">
+                      Email
+                    </label>
+                    <span>
+                      <input type="email" placeholder="Seu e-mail cadastrado" />
+                    </span>
+                  </span>
+                </form>
               </div>
             </div>
           </div>
