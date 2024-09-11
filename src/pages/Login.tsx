5e61de7d418ff6ecefa83779d504f7cb378d2fb1
César Filipe António <cesarfilipeantonio2404@gmail.com>
2024-09-11 23:01:17 +0100

    feat: install lucide-react e finalizei a estrutura do login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,3 +1,5 @@
+import { Eye, KeyRound, Mail, MoveRight } from "lucide-react";
+
 export function Login() {
   return (
     <div className="h-screen">
@@ -14,9 +16,9 @@ export function Login() {
               />
             </figure>
           </div>
-          <div className="p-6 max-w-2xl bg-white rounded-[32px]">
-            <div className="px-20 py-[72px]">
-              <div className="space-y-8">
+          <div className="m-6 max-w-2xl bg-white rounded-[32px]">
+            <div className="px-20 py-8 space-y-12">
+              <div className="space-y-12">
                 <div className="space-y-2">
                   <h1 className="text-2xl text-[#1D1D1D] font-semibold">
                     Acesse sua conta
@@ -25,16 +27,53 @@ export function Login() {
                     Informe seu e-mail e senha para entrar
                   </h2>
                 </div>
-                <form action="">
-                  <span className="flex flex-col">
-                    <label htmlFor="" className="text-[#666666] text-xs">
+                <form action="" className="space-y-12">
+                  <div className="space-y-5">
+                    <label
+                      htmlFor=""
+                      className="flex flex-col text-[#666666] text-xs"
+                    >
                       Email
+                      <span className="flex gap-2 px-2 py-2 border border-r-0 border-t-0 border-l-0 border-[#666666]">
+                        <Mail className="size-6 text-[#949494]" />
+                        <input
+                          type="email"
+                          placeholder="Seu e-mail cadastrado"
+                          className="text-base flex-1 outline-none text-[#949494]"
+                        />
+                      </span>
                     </label>
-                    <span>
-                      <input type="email" placeholder="Seu e-mail cadastrado" />
-                    </span>
-                  </span>
+
+                    <label
+                      htmlFor=""
+                      className="flex flex-col text-[#666666] text-xs"
+                    >
+                      Senha
+                      <span className="flex gap-2 px-2 py-2 border border-r-0 border-t-0 border-l-0 border-[#666666]">
+                        <KeyRound className="size-6 text-[#949494]" />
+                        <input
+                          type="email"
+                          placeholder="Sua senha de acesso"
+                          className="text-base flex-1 outline-none text-[#949494]"
+                        />
+                        <Eye className="size-6 text-[#666666]" />
+                      </span>
+                    </label>
+                  </div>{" "}
+                  <button className="px-5 rounded-xl h-14 flex justify-between bg-[#F24D0D] w-full items-center text-white">
+                    <span>Acessar</span>
+                    <MoveRight className="size-6" />
+                  </button>
                 </form>
+              </div>
+              <div className="space-y-3">
+                <h3 className="text-base text-[#666666]">
+                  Ainda não tem uma conta?
+                </h3>
+                <button className="px-5 rounded-xl h-14 flex justify-between border border-[#F24D0D] w-full items-center text-[#F24D0D]">
+                  <span>Acessar</span>
+                  <MoveRight className="size-6" />
+                </button>
               </div>
             </div>
           </div>
