9cf03d4ef8a5c7ae9e0ef24571cd69a961947a48
César Filipe António <cesarfilipeantonio2404@gmail.com>
2024-09-13 20:26:05 +0100

    chore:estilizando input

--- src/components/FormLogin.tsx
+++ src/components/FormLogin.tsx
@@ -2,13 +2,14 @@ import { Eye, KeyRound, Mail, MoveRight } from "lucide-react";
 
 export function FormLogin() {
   return (
-    <form action="" className="space-y-12">
+    <form className="space-y-12">
       <div className="space-y-5">
         <label htmlFor="" className="flex flex-col text-[#666666] text-xs">
           Email
           <span className="flex gap-2 px-2 py-2 border border-r-0 border-t-0 border-l-0 border-[#666666]">
             <Mail className="size-6 text-[#949494]" />
             <input
+              name="email"
               type="email"
               placeholder="Seu e-mail cadastrado"
               className="text-base flex-1 outline-none text-[#949494]"
@@ -21,7 +22,8 @@ export function FormLogin() {
           <span className="flex gap-2 px-2 py-2 border border-r-0 border-t-0 border-l-0 border-[#666666]">
             <KeyRound className="size-6 text-[#949494]" />
             <input
-              type="email"
+              name="password"
+              type="password"
               placeholder="Sua senha de acesso"
               className="text-base flex-1 outline-none text-[#949494]"
             />
@@ -29,7 +31,7 @@ export function FormLogin() {
           </span>
         </label>
       </div>
-      <button className="px-5 rounded-xl h-14 flex justify-between bg-[#F24D0D] w-full items-center text-white">
+      <button className="px-5 rounded-xl h-14 flex justify-between bg-[#F24D0D] hover:bg-[#C43C08] w-full items-center text-white">
         <span>Acessar</span>
         <MoveRight className="size-6" />
       </button>
--- src/pages/CardLogin.tsx
+++ src/pages/CardLogin.tsx
@@ -1,4 +1,5 @@
 import { MoveRight } from "lucide-react";
+import { FormLogin } from "../components/FormLogin";
 
 export function CardLogin() {
   return (
@@ -15,12 +16,13 @@ export function CardLogin() {
               </h2>
             </div>
           </div>
+          <FormLogin />
           <div className="space-y-3">
             <h3 className="text-base text-[#666666]">
               Ainda não tem uma conta?
             </h3>
-            <button className="px-5 rounded-xl h-14 flex justify-between border border-[#F24D0D] w-full items-center text-[#F24D0D]">
-              <span>Acessar</span>
+            <button className="px-5 rounded-xl h-14 flex justify-between border border-[#F24D0D] hover:bg-[#FBF4F4] w-full items-center text-[#F24D0D]">
+              <span>Cadastrar</span>
               <MoveRight className="size-6" />
             </button>
           </div>
