069e0d3959529ec21d83f82776403ea3664120b3
César Filipe António <cesarfilipeantonio2404@gmail.com>
2024-09-17 23:43:32 +0100

    chore:adicionando title no produtos

--- src/pages/Produtos.tsx
+++ src/pages/Produtos.tsx
@@ -2,8 +2,16 @@ import { Header } from "../components/Header";
 
 export function Produtos() {
   return (
-    <div>
+    <div className="space-y-12">
       <Header />
+      <div className="max-w-5xl flex flex-col justify-center items-center">
+        <div className="text-start">
+          <h1 className="text-2xl font-bold text-[#1D1D1D]">Seus produtos</h1>
+          <h2 className="text-lg text-[#666666]">
+            Acesse gerencie a sua lista de produtos à venda
+          </h2>
+        </div>
+      </div>
     </div>
   );
 }
