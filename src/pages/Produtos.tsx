f52e708f218e70720e1400887d34e4111a0aa1ae
César Filipe António <cesarfilipeantonio2404@gmail.com>
2024-09-18 22:57:45 +0100

    feat:add select

--- src/pages/Produtos.tsx
+++ src/pages/Produtos.tsx
@@ -1,15 +1,41 @@
+import { ChevronDown, Search, Tag } from "lucide-react";
 import { Header } from "../components/Header";
+import { Input } from "../components/Input";
 
 export function Produtos() {
   return (
     <div className="space-y-12">
       <Header />
-      <div className="max-w-5xl flex flex-col justify-center items-center">
+      <div className="max-w-5xl flex gap-8 flex-col justify-center items-center">
         <div className="text-start">
-          <h1 className="text-2xl font-bold text-[#1D1D1D]">Seus produtos</h1>
+          <h1 className="text-2xl font-bold text-[rgb(29,29,29)]">
+            Seus produtos
+          </h1>
           <h2 className="text-lg text-[#666666]">
             Acesse gerencie a sua lista de produtos à venda
           </h2>
+        </div>
+        <div className="flex gap-6">
+          <div className="p-6 space-y-6 rounded-[20px] bg-white">
+            <h2 className="text-lg text-[#666666]">Filtrar</h2>
+            <div>
+              <form>
+                <Input
+                  Icon={Search}
+                  type="text"
+                  name="seach"
+                  placeholder="Pesquisar"
+                />
+                <div className="flex justify-between px-2 py-[14px] border border-r-0 border-t-0 border-l-0 border-[#666666] text-[#949494] cursor-pointer">
+                  <div className="flex gap-2">
+                    <Tag className="size-6" />
+                    <span>Status</span>
+                  </div>
+                  <ChevronDown className="size-6" />
+                </div>
+              </form>
+            </div>
+          </div>
         </div>
       </div>
     </div>
